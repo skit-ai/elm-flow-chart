d525a26aea03f3fff0eee7a46e697939c4b63c80
Deepankar Agrawal <deepankar11093@gmail.com>
2019-05-13 21:57:51 +0530

    add encoders and decoders for types

---
+++ src/FlowChart/Json.elm
@@ -0,0 +1,117 @@
+module FlowChart.Json exposing
+    ( encodeVector2, encodeFCNode, encodeFCPort, encodeFCLink
+    , vector2Decoder, fcNodeDecoder, fcPortDecoder, fcLinkDecoder
+    )
+
+{-| Encoders and decoders for used data types
+
+
+# Encoders
+
+@docs encodeVector2, encodeFCNode, encodeFCPort, encodeFCLink
+
+
+# Decoders
+
+@docs vector2Decoder, fcNodeDecoder, fcPortDecoder, fcLinkDecoder
+
+-}
+
+import FlowChart.Types exposing (..)
+import Json.Decode as Decode
+import Json.Encode as Encode
+
+
+
+-- ENCODERS
+
+
+encodeVector2 : Vector2 -> Encode.Value
+encodeVector2 vec =
+    Encode.object
+        [ ( "x", Encode.float vec.x )
+        , ( "y", Encode.float vec.y )
+        ]
+
+
+encodeFCPort : FCPort -> Encode.Value
+encodeFCPort fcPort =
+    Encode.object
+        [ ( "id", Encode.string fcPort.id )
+        , ( "position", encodeVector2 fcPort.position )
+        ]
+
+
+encodeFCNode : FCNode -> Encode.Value
+encodeFCNode fcNode =
+    Encode.object
+        [ ( "id", Encode.string fcNode.id )
+        , ( "position", encodeVector2 fcNode.position )
+        , ( "dim", encodeVector2 fcNode.dim )
+        , ( "nodeType", Encode.string fcNode.nodeType )
+        , ( "ports", Encode.list encodeFCPort fcNode.ports )
+        ]
+
+
+encodeFCLink : FCLink -> Encode.Value
+encodeFCLink fcLink =
+    Encode.object
+        [ ( "id", Encode.string fcLink.id )
+        , ( "from", encodeLinkPosition fcLink.from )
+        , ( "to", encodeLinkPosition fcLink.to )
+        ]
+
+
+
+-- DECODERS
+
+
+vector2Decoder : Decode.Decoder Vector2
+vector2Decoder =
+    Decode.map2 Vector2
+        (Decode.field "x" Decode.float)
+        (Decode.field "y" Decode.float)
+
+
+fcPortDecoder : Decode.Decoder FCPort
+fcPortDecoder =
+    Decode.map2 FCPort
+        (Decode.field "id" Decode.string)
+        (Decode.field "position" vector2Decoder)
+
+
+fcNodeDecoder : Decode.Decoder FCNode
+fcNodeDecoder =
+    Decode.map5 FCNode
+        (Decode.field "id" Decode.string)
+        (Decode.field "position" vector2Decoder)
+        (Decode.field "dim" vector2Decoder)
+        (Decode.field "nodeType" Decode.string)
+        (Decode.field "ports" (Decode.list fcPortDecoder))
+
+
+fcLinkDecoder : Decode.Decoder FCLink
+fcLinkDecoder =
+    Decode.map3 FCLink
+        (Decode.field "id" Decode.string)
+        (Decode.field "from" linkPositionDecoder)
+        (Decode.field "to" linkPositionDecoder)
+
+
+
+-- HELPERS
+
+
+encodeLinkPosition : { nodeId : String, portId : String } -> Encode.Value
+encodeLinkPosition lPos =
+    Encode.object
+        [ ( "nodeId", Encode.string lPos.nodeId )
+        , ( "portId", Encode.string lPos.portId )
+        ]
+
+
+linkPositionDecoder : Decode.Decoder { nodeId : String, portId : String }
+linkPositionDecoder =
+    Decode.map2 (\x -> \y -> { nodeId = x, portId = y })
+        (Decode.field "nodeId" Decode.string)
+        (Decode.field "portId" Decode.string)
